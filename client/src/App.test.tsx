50ec6f2b9b0d6bb6de034624b075acefd64702f4
Sajeenthan <sajeen.dev@gmail.com>
2022-06-15 23:26:06 +0200

    Configured basic testing

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { createGenerateClassName, StylesProvider } from '@mui/styles';
 import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import store from './state/store';
+import GlobalThemeProvider from './styles/global-theme.provider';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'c',
+});
+
+test('renders PRIME text in home screen', () => {
+  render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <StylesProvider generateClassName={generateClassName}>
+          <GlobalThemeProvider>
+            <CssBaseline />
+            <App />
+          </GlobalThemeProvider>
+        </StylesProvider>
+      </BrowserRouter>
+    </Provider>
+  );
+  const linkElement = screen.getByText(/PRIME/i);
   expect(linkElement).toBeInTheDocument();
 });
